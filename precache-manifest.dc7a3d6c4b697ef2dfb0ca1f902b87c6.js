fd3774e1b8507515c9cf04ad680f794b00a8b595
AlehZahorski <git3371m@gmail.com>
2020-03-03 14:36:41 +0100

    Updates

---
+++ precache-manifest.dc7a3d6c4b697ef2dfb0ca1f902b87c6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8730443a22d7e22e03fb3ea5fc4a807e",
+    "url": "/social_network/index.html"
+  },
+  {
+    "revision": "ab34cec4e30751fb91d0",
+    "url": "/social_network/static/css/main.486cf46f.chunk.css"
+  },
+  {
+    "revision": "50a784f411a12f31664d",
+    "url": "/social_network/static/js/2.681d4b0c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/social_network/static/js/2.681d4b0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab34cec4e30751fb91d0",
+    "url": "/social_network/static/js/main.770013de.chunk.js"
+  },
+  {
+    "revision": "79455aa49ab1f603d304",
+    "url": "/social_network/static/js/runtime-main.2c15ef13.js"
+  },
+  {
+    "revision": "4aeb92aaa08ff581f498e64351999fc7",
+    "url": "/social_network/static/media/comment.4aeb92aa.svg"
+  },
+  {
+    "revision": "a886374085fa17c6a8bce4d8d07ba52f",
+    "url": "/social_network/static/media/forum.a8863740.svg"
+  },
+  {
+    "revision": "47be60baa8a7281a95b735338d2d4b18",
+    "url": "/social_network/static/media/friends.47be60ba.svg"
+  },
+  {
+    "revision": "1da478d7783907feee48a1c8dcd8fa74",
+    "url": "/social_network/static/media/group.1da478d7.svg"
+  },
+  {
+    "revision": "3e58e688b277846a0612ee0389fd25c9",
+    "url": "/social_network/static/media/like.3e58e688.svg"
+  },
+  {
+    "revision": "400c4dbf960c3c8fa0a5374faf6e2485",
+    "url": "/social_network/static/media/messages.400c4dbf.svg"
+  },
+  {
+    "revision": "a222014823b13f7c628ca9e486d43d00",
+    "url": "/social_network/static/media/profile.a2220148.svg"
+  },
+  {
+    "revision": "dedaa37037811c06c3c46b2933ebcbdc",
+    "url": "/social_network/static/media/settings.dedaa370.svg"
+  },
+  {
+    "revision": "b3997ad89ba9097f36f694930aa64890",
+    "url": "/social_network/static/media/share.b3997ad8.svg"
+  }
+]);
